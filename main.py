550b88b48f1f0fa4aec37a886f292f1ec8115f3f
KIMSIHUN <tlgns14@nate.com>
2024-08-06 20:13:34 +0900

    🎨 작업후 파일제거 추가

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
+import os
 from langchain_teddynote.document_loaders import HWPLoader
 from flask import Flask, request, jsonify
 
-# Initialize Flask app
 app = Flask(__name__)
 
 @app.route('/generate', methods=['POST'])
 def api():
     file = request.files.get('file')
     if file:
-        # Save the uploaded file
         file_path = f"./{file.filename}"
-        file.save(file_path)
-        
-        # Load the HWP document
-        loader = HWPLoader(file_path)
-        docs = loader.load()
-        
-        # Extract the text (assuming single document for simplicity)
-        extracted_text = docs[0].page_content[:2000]
-        
-        # Return the extracted text as JSON response
-        return jsonify({"extracted_text": extracted_text})
+        try:
+            file.save(file_path)
+            loader = HWPLoader(file_path)
+            docs = loader.load()
+            extracted_text = docs[0].page_content[:2000]
+            response = jsonify({"extracted_text": extracted_text})
+        except Exception as e:
+            response = jsonify({"error": str(e)}), 500
+        finally:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        return response
     else:
         return jsonify({"error": "No file uploaded"}), 400
 
